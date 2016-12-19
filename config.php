0861b01a10a66cfb63a5f4a47ec84ac55eafa958
gotkrypto76 <maxwlang@me.com>
2016-12-19 17:55:29 -0600

    Push real config.php

--- config.php
+++ config.php
@@ -1,3 +1,24 @@
 <?php
-// soon
+$aGlobalConfig = array
+(
+	"appURL" => "http://virulent.pw/webtss", // Full url to WebTSS, no trailing slash or space.
+	"python2.7Location" => "/usr/bin/python2.7", // Path to python 2.7, no trailing slash or space.
+	"database" => array 
+	(
+		"host" => "localhost",
+		"port" => "3306",
+		"username" => "<DB USERNAME>",
+		"password" => "<DB PASSWORD>",
+		"database" => "webtss"
+	)
+);
+
+
+
+
+// Configuration pre-processing.
+// Don't touch this.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
