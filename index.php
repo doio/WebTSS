a48127b7a8883fd26e8e44618cd6397aef65269f
gotkrypto76 <maxwlang@me.com>
2016-12-19 21:26:36 -0600

    Remove meta author

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="WebTSS, an open-source SHSH management program for the web.">
-    <meta name="author" content="https://github.com/gotkrypto76/WebTSS">
 
     <title>WebTSS - Manage SHSH Blobs online</title>
 
